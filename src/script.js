407658e735875ffc87c5c65a9a3cc87dab9949b3
elty-annie <eltinangwarai@gmail.com>
2022-10-05 22:33:11 +0200

    added forecast html and css

--- src/script.js
+++ src/script.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat"];
+  days.forEach(function(day) {
+  forecastHTML =
+    forecastHTML +
+    `
+  <div class="col-2">
+      <div class="forecast-date">
+          ${day}
+      </div>
+      <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="45px"/>
+      <div class="weather-forecast-temperatures">
+         <span class="weather-forecast-temperature-max">
+          21° 
+         </span> 
+         <span class="weather-forecast-temperature-min">
+          13°
+         </span> 
+      </div>
+  </div>
+
+
+`;
+});
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -84,3 +113,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Cape Town");
+displayForecast();
